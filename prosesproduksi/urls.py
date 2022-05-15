f1f3ec7a537f4a7608b6a853cead37ead35cbc95
ignatius.henriyanto <ignatius.henriyanto@ui.ac.id>
2022-05-15 15:22:27 +0700

    implement list histori makanan

--- prosesproduksi/urls.py
+++ prosesproduksi/urls.py
@@ -6,4 +6,5 @@ app_name = 'prosesproduksi'
 urlpatterns = [
     path('', views.index, name='index'),
     path('histori_tanaman', views.histori_tanaman, name='histori_tanaman'),
+    path('histori_makanan', views.histori_makanan, name='histori_makanan'),
 ]
