0c518c269fd2b1079dfa1bbd5a7548007df9e4be
fathan.muhammad01 <fathan.muhammad01@ui.ac.id>
2022-05-25 22:02:11 +0700

    Add trigger histori produksi & histori tanaman to sql file

--- trigger.sql
+++ trigger.sql
@@ -54,3 +54,43 @@ CREATE TRIGGER trigger_upgrade_lumbung
     BEFORE INSERT ON transaksi_upgrade_lumbung
     FOR EACH ROW
 EXECUTE PROCEDURE upgrade_lumbung();
+
+CREATE OR REPLACE FUNCTION produksi_tanaman()
+    RETURNS trigger AS
+$$
+DECLARE
+    old_jumlah_bibit INTEGER;
+    email_pengguna VARCHAR(50);
+BEGIN
+    SELECT HT.email
+    INTO email_pengguna
+    FROM HISTORI_TANAMAN HT
+    WHERE NEW.email = HT.email;
+
+    SELECT KAMA.jumlah
+    INTO old_jumlah_bibit
+    FROM KOLEKSI_ASET_MEMILIKI_ASET KAMA
+    WHERE NEW.email = KAMA.id_koleksi_aset;
+
+    UPDATE KOLEKSI_ASET_MEMILIKI_ASET
+    SET jumlah = jumlah - NEW.jumlah
+    WHERE NEW.email = id_koleksi_aset;
+
+    UPDATE LUMBUNG_MEMILIKI_PRODUK
+    SET jumlah = jumlah + NEW.jumlah
+    WHERE id_lumbung = NEW.email;
+
+    UPDATE PENGGUNA
+    SET xp = xp+5*NEW.jumlah
+    WHERE email = NEW.email;
+
+    RETURN NEW;
+
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_produksi_tanaman
+    BEFORE INSERT ON histori_produksi
+    FOR EACH ROW
+EXECUTE PROCEDURE produksi_tanaman();
