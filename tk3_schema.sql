9a50f0b6067a3cbc906c77b48ce9fb46449fd801
ignatius.henriyanto <ignatius.henriyanto@ui.ac.id>
2022-05-26 20:37:11 +0700

    Add trigger for histori_produksi_makanan

--- tk3_schema.sql
+++ tk3_schema.sql
@@ -308,8 +308,6 @@ CREATE TABLE DEKORASI_MEMILIKI_HISTORI_PENJUALAN (
 CREATE OR REPLACE FUNCTION add_lumbung_dan_koleksi_aset()
 RETURNS trigger AS
 $$
-DECLARE
-email_baru VARCHAR(50);
 BEGIN
 INSERT INTO lumbung VALUES (NEW.email, 1, 0, 0);
 
@@ -323,3 +321,71 @@ CREATE OR REPLACE TRIGGER trigger_add_lumbung_dan_koleksi_aset
 AFTER INSERT ON pengguna
 FOR EACH ROW
 EXECUTE PROCEDURE add_lumbung_dan_koleksi_aset();
+
+CREATE OR REPLACE FUNCTION histori_makanan()
+RETURNS trigger AS
+$$
+DECLARE
+ID_produk_bahan VARCHAR(5);
+jumlah_bahan INT;
+jumlah_produk INT;
+xp_produksi INT;
+jumlah_bahan_dimiliki INT;
+ID_alat VARCHAR(5);
+BEGIN
+SELECT HP.jumlah, HP.xp INTO jumlah_produk, xp_produksi
+FROM histori_produksi HP
+WHERE HP.email=NEW.email AND HP.waktu_awal=NEW.waktu_awal;
+-- RAISE NOTICE 'jumlah %, xp %', jumlah_produk, xp_produksi;
+
+SELECT A.ID_aset INTO ID_alat
+FROM produksi P
+INNER JOIN koleksi_aset_memiliki_aset A
+ON A.ID_aset = P.id_alat_produksi
+WHERE A.id_koleksi_aset=NEW.email AND P.id_produk_makanan=NEW.ID_produk_makanan;
+IF (ID_alat IS NOT NULL) THEN 
+	FOR ID_produk_bahan, jumlah_bahan IN (SELECT id_produk, jumlah
+					FROM produk_dibutuhkan_oleh_produk_makanan PPM
+					WHERE id_produk_makanan=NEW.ID_produk_makanan)
+	LOOP
+		-- RAISE NOTICE 'Bahan: % Jumlah: %', ID_produk_bahan, jumlah_bahan;
+		SELECT jumlah INTO jumlah_bahan_dimiliki
+		FROM lumbung_memiliki_produk
+		WHERE id_lumbung=NEW.email AND id_produk=ID_produk_bahan;
+		
+		-- RAISE NOTICE 'jumlah_bahan*jumlah_produk %', jumlah_bahan*jumlah_produk;
+		IF (jumlah_bahan*jumlah_produk > COALESCE(jumlah_bahan_dimiliki, 0)) THEN
+			DELETE FROM histori_produksi WHERE email=NEW.email AND waktu_awal=NEW.waktu_awal;
+			-- RAISE EXCEPTION 'Error: Bahan kurang';
+			RETURN NULL;
+		ELSE
+			-- RAISE NOTICE 'Bahan cukup';
+			UPDATE lumbung_memiliki_produk
+			SET jumlah = jumlah - jumlah_bahan*jumlah_produk
+			WHERE id_lumbung=NEW.email AND id_produk=ID_produk_bahan;
+
+			-- IF NEW.id_produk_makanan=(SELECT id_produk 
+			-- 			FROM lumbung_memiliki_produk
+			-- 			WHERE id_lumbung=NEW.email AND id_produk=NEW.id_produk_makanan)
+			-- THEN
+			-- 	UPDATE lumbung_memiliki_produk
+			-- 	SET jumlah = jumlah + jumlah_produk
+			-- 	WHERE id_lumbung=NEW.email AND id_produk=NEW.id_produk_makanan;
+			-- ELSE
+			-- 	INSERT INTO lumbung_memiliki_produk VALUES (NEW.email, NEW.id_produk_makanan, jumlah_produk);
+			-- END IF;
+		END IF;
+	END LOOP;
+ELSE
+	DELETE FROM histori_produksi WHERE email=NEW.email AND waktu_awal=NEW.waktu_awal;
+	RETURN NULL;
+END IF;
+RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_add_histori_makanan
+BEFORE INSERT ON histori_produksi_makanan
+FOR EACH ROW
+EXECUTE PROCEDURE histori_makanan();
